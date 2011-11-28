638f6807e016176aa8a728269e9267c4872893b9
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-28 03:38:16 +0000

    Separate l10n post types for each post types (post, page, ...)
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@468384 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/l10n.php
+++ includes/l10n.php
@@ -1,8 +1,8 @@
 <?php
 
-add_action( 'init', 'bogo_add_l10n_custom_post_type' );
+add_action( 'init', 'bogo_add_l10n_custom_post_types', 11 );
 
-function bogo_add_l10n_custom_post_type() {
+function bogo_add_l10n_custom_post_types() {
 	$labels = array(
 		'name' => __( 'Translations', 'bogo' ),
 		'singular_name' => __( 'Translation', 'bogo' ),
@@ -12,17 +12,25 @@ function bogo_add_l10n_custom_post_type() {
 		'view_item' => __( 'View Translation', 'bogo' ),
 		'search_items' => __( 'Search Translations', 'bogo' ),
 		'not_found' => __( 'No translations found', 'bogo' ),
-		'not_found_in_trash' => __( 'No translations found in Trash', 'bogo' ) );
+		'not_found_in_trash' => __( 'No translations found in Trash', 'bogo' ),
+		'menu_name' => __( 'Translations', 'bogo' ) );
 
 	$supports = array( 'title', 'editor', 'author',
 		'thumbnail', 'excerpt', 'custom-fields', 'comments', 'revisions' );
 
-	$args = array(
+	register_post_type( 'post_l10n', array(
 		'labels' => $labels,
+		'supports' => $supports,
 		'show_ui' => true,
-		'supports' => $supports );
+		'show_in_menu' => 'edit.php' ) );
 
-	register_post_type( 'l10n', $args );
+	register_post_type( 'page_l10n', array(
+		'labels' => $labels,
+		'supports' => $supports,
+		'show_ui' => true,
+		'show_in_menu' => 'edit.php?post_type=page' ) );
+
+	do_action( 'bogo_add_l10n_custom_post_types' );
 }
 
 ?>
