410ff506f98d73ead5e171252434b1c8d6b80793
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-08 06:29:50 +0000

    Check only explicit $_GET['lang'] when cached.
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@486446 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bogo.php
+++ bogo.php
@@ -69,30 +69,29 @@ function bogo_locale( $locale ) {
 		if ( $closest = bogo_get_closest_locale( $_GET['lang'] ) )
 			$locale = $closest;
 
-	} elseif ( isset( $_POST['lang'] ) ) {
-		if ( $closest = bogo_get_closest_locale( $_POST['lang'] ) )
-			$locale = $closest;
+	} elseif ( ! ( defined( 'WP_CACHE' ) && WP_CACHE ) ) {
 
-	} elseif ( isset( $_COOKIE['lang'] ) ) {
-		if ( $closest = bogo_get_closest_locale( $_COOKIE['lang'] ) )
-			$locale = $closest;
-
-	} elseif ( is_user_logged_in() ) {
-		$locale_option = get_user_option( 'locale' );
-
-		if ( ! empty( $locale_option ) )
-			$locale = $locale_option;
-
-	} elseif ( isset( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) ) {
-		$langs = bogo_http_accept_languages();
-
-		foreach ( (array) $langs as $lang ) {
-			if ( bogo_languages( $lang ) ) {
-				$locale = $lang;
-				break;
-			} elseif ( 2 == strlen( $lang ) && $closest = bogo_get_closest_locale( $lang ) ) {
+		if ( isset( $_COOKIE['lang'] ) ) {
+			if ( $closest = bogo_get_closest_locale( $_COOKIE['lang'] ) )
 				$locale = $closest;
-				break;
+
+		} elseif ( is_user_logged_in() ) {
+			$locale_option = get_user_option( 'locale' );
+
+			if ( ! empty( $locale_option ) )
+				$locale = $locale_option;
+
+		} elseif ( isset( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) ) {
+			$langs = bogo_http_accept_languages();
+
+			foreach ( (array) $langs as $lang ) {
+				if ( bogo_languages( $lang ) ) {
+					$locale = $lang;
+					break;
+				} elseif ( 2 == strlen( $lang ) && $closest = bogo_get_closest_locale( $lang ) ) {
+					$locale = $closest;
+					break;
+				}
 			}
 		}
 	}
