a8714ee7c3a903990bbb3d72326d7a4769cad53e
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-13 02:39:34 +0000

    permalink filtering for page and custom post type
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@571544 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/link-template.php
+++ includes/link-template.php
@@ -9,10 +9,57 @@ function bogo_post_link( $permalink, $post, $leavename ) {
 	if ( $default_locale == $locale )
 		return $permalink;
 
+	$sample = ( isset( $post->filter ) && 'sample' == $post->filter );
+
 	$permalink_structure = get_option( 'permalink_structure' );
 
 	if ( empty( $permalink_structure )
-	|| in_array( $post->post_status, array( 'draft', 'pending', 'auto-draft' ) ) )
+	|| ! $sample && in_array( $post->post_status, array( 'draft', 'pending', 'auto-draft' ) ) )
+		return add_query_arg( array( 'lang' => $locale ), $permalink );
+
+	$permalink = bogo_get_url_with_lang( $permalink, $locale );
+
+	return $permalink;
+}
+
+add_filter( 'page_link', 'bogo_page_link', 10, 3 );
+
+function bogo_page_link( $permalink, $id, $sample ) {
+	if ( 'page' == get_option( 'show_on_front' ) && $id == get_option( 'page_on_front' ) )
+		return $permalink;
+
+	$default_locale = bogo_get_default_locale();
+	$locale = bogo_get_post_locale( $id );
+
+	if ( $default_locale == $locale )
+		return $permalink;
+
+	$post = get_post( $id );
+
+	$permalink_structure = get_option( 'permalink_structure' );
+
+	if ( empty( $permalink_structure )
+	|| ! $sample && in_array( $post->post_status, array( 'draft', 'pending', 'auto-draft' ) ) )
+		return add_query_arg( array( 'lang' => $locale ), $permalink );
+
+	$permalink = bogo_get_url_with_lang( $permalink, $locale );
+
+	return $permalink;
+}
+
+add_filter( 'post_type_link', 'bogo_post_type_link', 10, 4 );
+
+function bogo_post_type_link( $permalink, $post, $leavename, $sample ) {
+	$default_locale = bogo_get_default_locale();
+	$locale = bogo_get_post_locale( $post->ID );
+
+	if ( $default_locale == $locale )
+		return $permalink;
+
+	$permalink_structure = get_option( 'permalink_structure' );
+
+	if ( empty( $permalink_structure )
+	|| ! $sample && in_array( $post->post_status, array( 'draft', 'pending', 'auto-draft' ) ) )
 		return add_query_arg( array( 'lang' => $locale ), $permalink );
 
 	$permalink = bogo_get_url_with_lang( $permalink, $locale );
