4b6d612253f8d41553a07aa78a8a29149e1a4b00
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-25 05:12:56 +0000

    Posts without '_locale' meta should be treated as posts in default locale.
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@576960 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bogo.php
+++ bogo.php
@@ -100,6 +100,15 @@ function bogo_parse_query( $query ) {
 	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
 		return;
 
+	if ( isset( $qv['post_type'] ) && 'any' != $qv['post_type'] ) {
+		$localizable = array_filter( (array) $qv['post_type'], 'bogo_is_localizable_post_type' );
+
+		if ( empty( $localizable ) ) {
+			$qv['bogo_suppress_locale_query'] = true;
+			return;
+		}
+	}
+
 	$lang = isset( $qv['lang'] ) ? $qv['lang'] : '';
 
 	if ( is_admin() ) {
@@ -111,16 +120,63 @@ function bogo_parse_query( $query ) {
 			$locale = bogo_get_default_locale();
 	}
 
-	if ( empty( $locale ) )
-		return;
+	if ( empty( $locale ) || ! bogo_is_available_locale( $locale ) )
+		$qv['bogo_suppress_locale_query'] = true;
+	else
+		$qv['lang'] = $locale;
+}
 
-	$meta_query = array(
-		array( 'key' => '_locale', 'value' => $locale ) );
+add_filter( 'posts_join', 'bogo_posts_join', 10, 2 );
 
-	if ( ! isset( $qv['meta_query'] ) )
-		$qv['meta_query'] = array();
+function bogo_posts_join( $join, $query ) {
+	global $wpdb;
 
-	$qv['meta_query'] = array_merge( $qv['meta_query'], $meta_query );
+	$qv = &$query->query_vars;
+
+	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
+		return $join;
+
+	$locale = empty( $qv['lang'] ) ? '' : $qv['lang'];
+
+	if ( ! bogo_is_available_locale( $locale ) )
+		return $join;
+
+	if ( ! $meta_table = _get_meta_table( 'post' ) )
+		return $join;
+
+	$join .= " LEFT JOIN $meta_table AS postmeta_bogo ON ($wpdb->posts.ID = postmeta_bogo.post_id AND postmeta_bogo.meta_key = '_locale')";
+
+	return $join;
+}
+
+add_filter( 'posts_where', 'bogo_posts_where', 10, 2 );
+
+function bogo_posts_where( $where, $query ) {
+	global $wpdb;
+
+	$qv = &$query->query_vars;
+
+	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
+		return $where;
+
+	$locale = empty( $qv['lang'] ) ? '' : $qv['lang'];
+
+	if ( ! bogo_is_available_locale( $locale ) )
+		return $where;
+
+	if ( ! $meta_table = _get_meta_table( 'post' ) )
+		return $where;
+
+	$where .= " AND (1=0";
+
+	$where .= $wpdb->prepare( " OR postmeta_bogo.meta_value LIKE %s", $locale );
+
+	if ( bogo_is_default_locale( $locale ) )
+		$where .= " OR postmeta_bogo.meta_id IS NULL";
+
+	$where .= ")";
+
+	return $where;
 }
 
 ?>
