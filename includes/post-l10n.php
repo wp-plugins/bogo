bc1aea595176f5aa90d767e8e641452f3c16c4ea
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-18 08:56:59 +0000

    Add locale class to post_class.
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@757867 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/post-l10n.php
+++ includes/post-l10n.php
@@ -418,4 +418,17 @@ function bogo_body_class( $classes, $class ) {
 	return $classes;
 }
 
+add_filter( 'post_class', 'bogo_post_class', 10, 3 );
+
+function bogo_post_class( $classes, $class, $post_id ) {
+	$locale = bogo_get_post_locale( $post_id );
+	$locale = bogo_language_tag( $locale );
+	$locale = esc_attr( $locale );
+
+	if ( $locale && ! in_array( $locale, $classes ) )
+		$classes[] = $locale;
+
+	return $classes;
+}
+
 ?>
