b7c8c8da132d184cad68165427820a0110131a60
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-18 09:58:38 +0000

    Move query-related functions to query.php.
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@757874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bogo.php
+++ bogo.php
@@ -49,6 +49,7 @@ require_once BOGO_PLUGIN_DIR . '/includes/post-l10n-functions.php';
 require_once BOGO_PLUGIN_DIR . '/includes/user-l10n-functions.php';
 require_once BOGO_PLUGIN_DIR . '/includes/post-l10n.php';
 require_once BOGO_PLUGIN_DIR . '/includes/user-l10n.php';
+require_once BOGO_PLUGIN_DIR . '/includes/query.php';
 
 add_action( 'plugins_loaded', 'bogo_plugins_loaded' );
 
@@ -119,118 +120,6 @@ function bogo_query_vars( $query_vars ) {
 	$query_vars[] = 'lang';
 
 	return $query_vars;
-}
-
-add_action( 'parse_query', 'bogo_parse_query' );
-
-function bogo_parse_query( $query ) {
-	$qv = &$query->query_vars;
-
-	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
-		return;
-
-	if ( isset( $qv['post_type'] ) && 'any' != $qv['post_type'] ) {
-		$localizable = array_filter( (array) $qv['post_type'], 'bogo_is_localizable_post_type' );
-
-		if ( empty( $localizable ) ) {
-			$qv['bogo_suppress_locale_query'] = true;
-			return;
-		}
-	}
-
-	$lang = isset( $qv['lang'] ) ? $qv['lang'] : '';
-
-	if ( is_admin() ) {
-		$locale = $lang;
-	} else {
-		if ( $lang )
-			$locale = bogo_get_closest_locale( $lang );
-		else
-			$locale = get_locale();
-
-		if ( empty( $locale ) )
-			$locale = bogo_get_default_locale();
-	}
-
-	if ( empty( $locale ) || ! bogo_is_available_locale( $locale ) ) {
-		$qv['bogo_suppress_locale_query'] = true;
-		return;
-	}
-
-	$qv['lang'] = $locale;
-
-	if ( is_admin() )
-		return;
-
-	if ( '' != $qv['pagename'] ) {
-		$query->queried_object = bogo_get_page_by_path( $qv['pagename'], $locale );
-
-		if ( ! empty( $query->queried_object ) )
-			$query->queried_object_id = (int) $query->queried_object->ID;
-		else
-			unset( $query->queried_object );
-
-		if  ( 'page' == get_option( 'show_on_front' )
-		&& isset( $query->queried_object_id )
-		&& $query->queried_object_id == get_option( 'page_for_posts' ) ) {
-			$query->is_page = false;
-			$query->is_home = true;
-			$query->is_posts_page = true;
-		}
-	}
-}
-
-add_filter( 'posts_join', 'bogo_posts_join', 10, 2 );
-
-function bogo_posts_join( $join, $query ) {
-	global $wpdb;
-
-	$qv = &$query->query_vars;
-
-	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
-		return $join;
-
-	$locale = empty( $qv['lang'] ) ? '' : $qv['lang'];
-
-	if ( ! bogo_is_available_locale( $locale ) )
-		return $join;
-
-	if ( ! $meta_table = _get_meta_table( 'post' ) )
-		return $join;
-
-	$join .= " LEFT JOIN $meta_table AS postmeta_bogo ON ($wpdb->posts.ID = postmeta_bogo.post_id AND postmeta_bogo.meta_key = '_locale')";
-
-	return $join;
-}
-
-add_filter( 'posts_where', 'bogo_posts_where', 10, 2 );
-
-function bogo_posts_where( $where, $query ) {
-	global $wpdb;
-
-	$qv = &$query->query_vars;
-
-	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
-		return $where;
-
-	$locale = empty( $qv['lang'] ) ? '' : $qv['lang'];
-
-	if ( ! bogo_is_available_locale( $locale ) )
-		return $where;
-
-	if ( ! $meta_table = _get_meta_table( 'post' ) )
-		return $where;
-
-	$where .= " AND (1=0";
-
-	$where .= $wpdb->prepare( " OR postmeta_bogo.meta_value LIKE %s", $locale );
-
-	if ( bogo_is_default_locale( $locale ) )
-		$where .= " OR postmeta_bogo.meta_id IS NULL";
-
-	$where .= ")";
-
-	return $where;
 }
 
 ?>
---
+++ includes/query.php
@@ -0,0 +1,115 @@
+<?php
+
+add_action( 'parse_query', 'bogo_parse_query' );
+
+function bogo_parse_query( $query ) {
+	$qv = &$query->query_vars;
+
+	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
+		return;
+
+	if ( isset( $qv['post_type'] ) && 'any' != $qv['post_type'] ) {
+		$localizable = array_filter( (array) $qv['post_type'], 'bogo_is_localizable_post_type' );
+
+		if ( empty( $localizable ) ) {
+			$qv['bogo_suppress_locale_query'] = true;
+			return;
+		}
+	}
+
+	$lang = isset( $qv['lang'] ) ? $qv['lang'] : '';
+
+	if ( is_admin() ) {
+		$locale = $lang;
+	} else {
+		if ( $lang )
+			$locale = bogo_get_closest_locale( $lang );
+		else
+			$locale = get_locale();
+
+		if ( empty( $locale ) )
+			$locale = bogo_get_default_locale();
+	}
+
+	if ( empty( $locale ) || ! bogo_is_available_locale( $locale ) ) {
+		$qv['bogo_suppress_locale_query'] = true;
+		return;
+	}
+
+	$qv['lang'] = $locale;
+
+	if ( is_admin() )
+		return;
+
+	if ( '' != $qv['pagename'] ) {
+		$query->queried_object = bogo_get_page_by_path( $qv['pagename'], $locale );
+
+		if ( ! empty( $query->queried_object ) )
+			$query->queried_object_id = (int) $query->queried_object->ID;
+		else
+			unset( $query->queried_object );
+
+		if  ( 'page' == get_option( 'show_on_front' )
+		&& isset( $query->queried_object_id )
+		&& $query->queried_object_id == get_option( 'page_for_posts' ) ) {
+			$query->is_page = false;
+			$query->is_home = true;
+			$query->is_posts_page = true;
+		}
+	}
+}
+
+add_filter( 'posts_join', 'bogo_posts_join', 10, 2 );
+
+function bogo_posts_join( $join, $query ) {
+	global $wpdb;
+
+	$qv = &$query->query_vars;
+
+	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
+		return $join;
+
+	$locale = empty( $qv['lang'] ) ? '' : $qv['lang'];
+
+	if ( ! bogo_is_available_locale( $locale ) )
+		return $join;
+
+	if ( ! $meta_table = _get_meta_table( 'post' ) )
+		return $join;
+
+	$join .= " LEFT JOIN $meta_table AS postmeta_bogo ON ($wpdb->posts.ID = postmeta_bogo.post_id AND postmeta_bogo.meta_key = '_locale')";
+
+	return $join;
+}
+
+add_filter( 'posts_where', 'bogo_posts_where', 10, 2 );
+
+function bogo_posts_where( $where, $query ) {
+	global $wpdb;
+
+	$qv = &$query->query_vars;
+
+	if ( ! empty( $qv['bogo_suppress_locale_query'] ) )
+		return $where;
+
+	$locale = empty( $qv['lang'] ) ? '' : $qv['lang'];
+
+	if ( ! bogo_is_available_locale( $locale ) )
+		return $where;
+
+	if ( ! $meta_table = _get_meta_table( 'post' ) )
+		return $where;
+
+	$where .= " AND (1=0";
+
+	$where .= $wpdb->prepare( " OR postmeta_bogo.meta_value LIKE %s", $locale );
+
+	if ( bogo_is_default_locale( $locale ) )
+		$where .= " OR postmeta_bogo.meta_id IS NULL";
+
+	$where .= ")";
+
+	return $where;
+}
+
+?>
