19adc9fb4955e486b2f5f2f770d93e48a2d7097c
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-18 11:59:43 +0000

    Apply widget language selector the same format as menu's one.
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@916527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/widgets.php
+++ includes/widgets.php
@@ -75,20 +75,29 @@ add_action( 'in_widget_form', 'bogo_in_widget_form', 10, 3 );
 function bogo_in_widget_form( $widget, $return, $instance ) {
 	$available_languages = bogo_available_languages( 'orderby=value' );
 
-	$selected_languages = ! isset( $instance['bogo_locales'] )
-		? array_keys( $available_languages ) : (array) $instance['bogo_locales'];
+	if ( empty( $available_languages ) ) {
+		return;
+	}
+
+	$return = null;
+
+	$selected_languages = isset( $instance['bogo_locales'] )
+		? (array) $instance['bogo_locales']
+		: array_keys( $available_languages );
 
 ?>
-<fieldset style="padding: .4em 1em .6em; border: 1px solid #ccc;">
-<legend style="padding: 0 1em;"><?php echo esc_html( __( 'Displayed on pages in:', 'bogo' ) ); ?></legend>
-<?php foreach ( $available_languages as $locale => $language ) : ?>
-<input type="checkbox" id="<?php echo $widget->get_field_id( 'bogo_locales' ); ?>" name="<?php echo $widget->get_field_name( 'bogo_locales' ); ?>[]" value="<?php echo esc_attr( $locale ); ?>"<?php echo in_array( $locale, $selected_languages ) ? ' checked="checked"' : ''; ?> />
-	<?php echo esc_html( $language ); ?><br />
+<fieldset class="bogo-locale-options">
+<legend><?php echo esc_html( __( 'Displayed on pages in:', 'bogo' ) ); ?></legend>
+<?php foreach ( $available_languages as $locale => $language ) :
+	$checked = in_array( $locale, $selected_languages );
+	$id_attr = $widget->get_field_id( 'bogo_locales' ) . '-' . $locale;
+?>
+<label class="bogo-locale-option<?php echo $checked ? ' checked' : ''; ?>" for="<?php echo $id_attr; ?>">
+<input type="checkbox" id="<?php echo $id_attr; ?>" name="<?php echo $widget->get_field_name( 'bogo_locales' ); ?>[]" value="<?php echo esc_attr( $locale ); ?>"<?php echo $checked ? ' checked="checked"' : ''; ?> /><?php echo esc_html( $language ); ?>
+</label>
 <?php endforeach; ?>
 </fieldset>
 <?php
-
-	$return = null;
 }
 
 add_filter( 'widget_update_callback', 'bogo_widget_update_callback', 10, 4 );
