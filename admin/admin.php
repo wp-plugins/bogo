9b8ba4fb54a6de139f76059b5d14bd8e9cc800ed
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-04 00:14:35 +0000

    Create admin directory.
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@925984 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin.php
@@ -0,0 +1,44 @@
+<?php
+
+add_action( 'admin_enqueue_scripts', 'bogo_admin_enqueue_scripts' );
+
+function bogo_admin_enqueue_scripts( $hook_suffix ) {
+	if ( 'widgets.php' == $hook_suffix ) {
+		wp_enqueue_style( 'bogo-admin',
+			plugins_url( 'includes/admin.css', BOGO_PLUGIN_BASENAME ),
+			array(), BOGO_VERSION, 'all' );
+
+		return;
+	}
+
+	if ( 'nav-menus.php' == $hook_suffix ) {
+		$nav_menu_id = absint( get_user_option( 'nav_menu_recently_edited' ) );
+		$nav_menu_items = wp_get_nav_menu_items( $nav_menu_id );
+		$locales = array();
+
+		foreach ( (array) $nav_menu_items as $item ) {
+			$locales[$item->db_id] = $item->bogo_locales;
+		}
+
+		$prefix = 'menu-item-bogo-locale';
+
+		wp_enqueue_script( 'bogo-admin',
+			plugins_url( 'includes/admin.js', BOGO_PLUGIN_BASENAME ),
+			array( 'jquery' ),
+			BOGO_VERSION, true );
+
+		wp_localize_script( 'bogo-admin', '_bogo', array(
+			'availableLanguages' => bogo_available_languages( 'orderby=value' ),
+			'locales' => $locales,
+			'selectorLegend' => __( 'Displayed on pages in', 'bogo' ),
+			'cbPrefix' => $prefix ) );
+
+		wp_enqueue_style( 'bogo-admin',
+			plugins_url( 'includes/admin.css', BOGO_PLUGIN_BASENAME ),
+			array(), BOGO_VERSION, 'all' );
+
+		return;
+	}
+}
+
+?>
