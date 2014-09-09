caf99196e9ec28b4c4df3fd2666d1854b2b04fc4
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-09 11:17:59 +0000

    Restructure under admin/
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@985242 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin.php
+++ admin/admin.php
@@ -10,7 +10,7 @@ add_action( 'admin_enqueue_scripts', 'bogo_admin_enqueue_scripts' );
 function bogo_admin_enqueue_scripts( $hook_suffix ) {
 	if ( 'widgets.php' == $hook_suffix || 'user-edit.php' == $hook_suffix ) {
 		wp_enqueue_style( 'bogo-admin',
-			plugins_url( 'admin/admin.css', BOGO_PLUGIN_BASENAME ),
+			plugins_url( 'admin/includes/css/admin.css', BOGO_PLUGIN_BASENAME ),
 			array(), BOGO_VERSION, 'all' );
 
 		return;
@@ -28,7 +28,7 @@ function bogo_admin_enqueue_scripts( $hook_suffix ) {
 		$prefix = 'menu-item-bogo-locale';
 
 		wp_enqueue_script( 'bogo-admin',
-			plugins_url( 'admin/admin.js', BOGO_PLUGIN_BASENAME ),
+			plugins_url( 'admin/includes/js/admin.js', BOGO_PLUGIN_BASENAME ),
 			array( 'jquery' ),
 			BOGO_VERSION, true );
 
@@ -39,7 +39,7 @@ function bogo_admin_enqueue_scripts( $hook_suffix ) {
 			'cbPrefix' => $prefix ) );
 
 		wp_enqueue_style( 'bogo-admin',
-			plugins_url( 'admin/admin.css', BOGO_PLUGIN_BASENAME ),
+			plugins_url( 'admin/includes/css/admin.css', BOGO_PLUGIN_BASENAME ),
 			array(), BOGO_VERSION, 'all' );
 
 		return;
