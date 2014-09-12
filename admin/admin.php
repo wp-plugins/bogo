28f2fc95d9a102f0c51d9cf19f4629a9cb5213f0
takayukister <takayukister@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-12 14:16:25 +0000

    Bogo Tools page: styling tweak.
    git-svn-id: https://plugins.svn.wordpress.org/bogo/trunk@988307 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin.php
+++ admin/admin.php
@@ -8,7 +8,9 @@ require_once BOGO_PLUGIN_DIR . '/admin/includes/widgets.php';
 add_action( 'admin_enqueue_scripts', 'bogo_admin_enqueue_scripts' );
 
 function bogo_admin_enqueue_scripts( $hook_suffix ) {
-	if ( 'widgets.php' == $hook_suffix || 'user-edit.php' == $hook_suffix ) {
+	if ( false !== strpos( $hook_suffix, 'bogo-tools' )
+	|| 'widgets.php' == $hook_suffix
+	|| 'user-edit.php' == $hook_suffix ) {
 		wp_enqueue_style( 'bogo-admin',
 			plugins_url( 'admin/includes/css/admin.css', BOGO_PLUGIN_BASENAME ),
 			array(), BOGO_VERSION, 'all' );
@@ -50,7 +52,7 @@ add_action( 'admin_menu', 'bogo_admin_menu' );
 
 function bogo_admin_menu() {
 	$tools = add_management_page(
-		__( 'Translations', 'bogo' ), __( 'Translations', 'bogo' ),
+		__( 'Bogo Tools', 'bogo' ), __( 'Bogo', 'bogo' ),
 		'update_core', 'bogo-tools', 'bogo_tools_page' );
 
 	add_action( 'load-' . $tools, 'bogo_load_tools_page' );
@@ -86,12 +88,6 @@ function bogo_load_tools_page() {
 }
 
 function bogo_tools_page() {
-	$default_locale = bogo_get_default_locale();
-	$languages = array( $default_locale => bogo_get_language( $default_locale ) )
-		+ bogo_available_languages() + bogo_languages();
-	$can_install = wp_can_install_language_pack();
-	$available_translations = wp_get_available_translations();
-
 	$message = "";
 
 	if ( isset( $_GET['message'] ) ) {
@@ -105,26 +101,64 @@ function bogo_tools_page() {
 ?>
 <div class="wrap">
 
-<h2><?php echo esc_html( __( 'Translations', 'bogo' ) ); ?></h2>
+<h2><?php echo esc_html( __( 'Bogo Tools', 'bogo' ) ); ?></h2>
 
 <?php if ( ! empty( $message ) ) : ?>
 <div id="message" class="updated"><p><?php echo esc_html( $message ); ?></p></div>
 <?php endif; ?>
 
-<ul>
+<?php
+	bogo_toolbox_installed_translations();
+	bogo_toolbox_add_translations();
+?>
+
+</div>
+<?php
+}
+
+function bogo_toolbox_installed_translations() {
+	$title = __( 'Available Languages', 'bogo' );
+
+	$default_locale = bogo_get_default_locale();
+	$languages = array( $default_locale => bogo_get_language( $default_locale ) )
+		+ bogo_available_languages();
+?>
+<div class="tool-box">
+<h3 class="title"><?php echo esc_html( $title ); ?></h3>
+<ul id="translations">
 <?php
 	foreach ( $languages as $locale => $language ) {
-		if ( bogo_is_available_locale( $locale ) ) {
-			echo sprintf( '<li><strong>%s</strong></li>', esc_html( $language ) );
-		} elseif ( ! $can_install ) {
-			echo sprintf( '<li>%s</li>', esc_html( $language ) );
-		} elseif ( isset( $available_translations[$locale] ) ) {
+		echo sprintf( '<li>%s</li>', esc_html( $language ) );
+	}
+?>
+</ul>
+</div>
+<?php
+}
+
+function bogo_toolbox_add_translations() {
+	if ( ! wp_can_install_language_pack() ) {
+		return;
+	}
+
+	$title = __( 'Add Languages', 'bogo' );
+
+	$available_translations = wp_get_available_translations();
+	$languages = array_diff( bogo_languages(), bogo_available_languages() );
+
+?>
+<div class="tool-box">
+<h3 class="title"><?php echo esc_html( $title ); ?></h3>
+<ul id="translations">
+<?php
+	foreach ( $languages as $locale => $language ) {
+		if ( isset( $available_translations[$locale] ) ) {
 			$install_link = menu_page_url( 'bogo-tools', false );
 			$install_link = add_query_arg(
 				array( 'action' => 'install_translation', 'locale' => $locale ),
 				$install_link );
 			$install_link = wp_nonce_url( $install_link, 'bogo-tools' );
-			$install_link = sprintf( '<a href="%1$s">%2$s</a>',
+			$install_link = sprintf( '<a href="%1$s" class="install">%2$s</a>',
 				$install_link, __( 'Install', 'bogo' ) );
 
 			echo sprintf( '<li>%1$s %2$s</li>',
